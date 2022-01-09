a0da4e74b91833fe3bc911fba9ceba6be509cfad
tuhinbose <45883876+tuhinbose@users.noreply.github.com>
2022-01-09 13:27:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ st.title("Build and Deploy Stock Market App Using Streamlit")
 st.header("A Basic Data Science Web Application")
 st.sidebar.header("Geeksforgeeks \n TrueGeeks")
 
-company1 = get_ticker("GOOGL")
-company2 = get_ticker("MSFT")
+company1 = get_ticker("IRCTC.NS")
+company2 = get_ticker("INFY.NS")
 
 # fetches the data: Open, Close, High, Low and Volume
-google = finance.download("GOOGL", start="2020-12-01", end="2021-01-08")
-microsoft = finance.download("MSFT", start="2020-12-01", end="2021-01-08")
+google = finance.download("IRCTC.NS", start="2020-12-01", end="2021-01-08")
+microsoft = finance.download("INFY.NS", start="2020-12-01", end="2021-01-08")
 
 # Valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
 data1 = company1.history(period="3mo")
@@ -30,7 +30,7 @@ st.write("""
 
 # detailed summary on Google
 st.write(company1.info['longBusinessSummary'])
-st.write(google)
+st.write(IRCTC)
 
 # plots the graph
 st.line_chart(data1.values)
@@ -38,5 +38,5 @@ st.line_chart(data1.values)
 st.write("""
 ### Microsoft
 """)
-st.write(company2.info['longBusinessSummary'], "\n", microsoft)
+st.write(company2.info['longBusinessSummary'], "\n", INFOSYS)
 st.line_chart(data2.values)
